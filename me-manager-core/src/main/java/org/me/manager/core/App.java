cadb4e03658d4bb93cc22d006b12b23c29d1b552
yaoxiangchun <1097037585@qq.com>
2020-04-04 12:49:48 +0800

    first commit

---
+++ me-manager-core/src/main/java/org/me/manager/core/App.java
@@ -0,0 +1,13 @@
+package org.me.manager.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
