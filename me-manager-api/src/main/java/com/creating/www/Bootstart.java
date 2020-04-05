525e3c9cdb89d5b7e9c931612fdb7192030e8bc2
yaoxiangchun <1097037585@qq.com>
2020-04-05 18:43:25 +0800

    1.update modules and spring-boot ref jars

---
+++ me-manager-api/src/main/java/com/creating/www/Bootstart.java
@@ -0,0 +1,13 @@
+package com.creating.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootstart {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootstart.class, args);
+	}
+
+}
---
+++ me-manager-config/src/main/java/com/creating/www/FilterConfig.java
@@ -0,0 +1,5 @@
+package com.creating.www;
+
+public class FilterConfig {
+
+}
