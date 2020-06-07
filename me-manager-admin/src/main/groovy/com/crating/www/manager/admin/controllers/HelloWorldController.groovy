c4342593b1a2438d96600ada4d710f9a0e01b536
hack0303 <1097037585@qq.com>
2020-06-07 14:29:09 +0800

    1.add groovy

---
+++ me-manager-admin/src/main/groovy/com/crating/www/manager/admin/controllers/HelloWorldController.groovy
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.me.manager.admin.controller
+
+import lombok.extern.slf4j.Slf4j
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ *
+ * @author Administrator
+ */
+@Slf4j
+@RestController
+class HelloWorldController {
+
+    @GetMapping("hello-world")
+    public String showHelloWorld(){
+        return "hello world";
+    }
+}
+
