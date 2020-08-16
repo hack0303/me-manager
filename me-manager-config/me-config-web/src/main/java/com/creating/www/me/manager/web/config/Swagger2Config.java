fe142c146155a541d44a16d1ce16daa59e0714a3
hack0303 <1097037585@qq.com>
2020-08-17 02:33:04 +0800

    + swagger2

--- me-manager-config/me-config-web/src/main/java/com/creating/www/me/manager/web/config/Swagger2Config.java
+++ me-manager-config/me-config-web/src/main/java/com/creating/www/me/manager/web/config/Swagger2Config.java
@@ -2,27 +2,66 @@ package com.creating.www.me.manager.web.config;
 
 import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
 
 @EnableSwagger2
 @EnableSwaggerBootstrapUI
 @Profile("dev")
+@Configuration
 public class Swagger2Config {
 
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(ApiInfo.DEFAULT)
+                .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any()) // 可以根据url路径设置哪些请求加入文档，忽略哪些请求
+                .build()
+                .securitySchemes(securitySchemes())
+                .securityContexts(securityContexts());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("松鼠平台接口文档")
+                .description("无")
+                .version("v2.4.0")
+                .contact(new Contact("MOB", "无", "无"))
                 .build();
     }
 
+    private List<ApiKey> securitySchemes() {
+        return Arrays.asList(
+                new ApiKey("Authorization", "token", "header"));
+    }
+
+    private List<SecurityContext> securityContexts() {
+        return Arrays.asList(
+                SecurityContext.builder()
+                        .securityReferences(defaultAuth())
+                        .forPaths(PathSelectors.regex("^(?!auth).*$"))
+                        .build()
+        );
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(
+                new SecurityReference("Authorization", authorizationScopes));
+    }
 }
